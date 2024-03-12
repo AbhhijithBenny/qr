52ef2e325a2ab66d5f8cd074d33e0821f0a7159a
abhijith benny <abhijithbenny04@gmail.com>
2024-03-12 15:26:13 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const QrApp());
+}
+class QrApp  extends StatefulWidget {
+  const ({super.key});
+
+  @override
+  State<> createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
